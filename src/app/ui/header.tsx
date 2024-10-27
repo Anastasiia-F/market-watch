1dc36654f2a51caaca4b5ca1c0e2930467595d89
Anastasiia Fesenko <afesenk@gmail.com>
2024-10-27 18:46:58 +0100

    add Blog page, add link for logo

--- src/app/ui/header.tsx
+++ src/app/ui/header.tsx
@@ -3,6 +3,7 @@ import { Image } from 'primereact/image';
 import { Menubar } from 'primereact/menubar';
 import UserMenu from '@/app/ui/userMenu/userMenu';
 import HeaderSearch from '@/app/ui/headrSearch/headrSearch';
+import Link from 'next/link';
 
 const Header = () => {
     const items = [
@@ -12,7 +13,7 @@ const Header = () => {
         },
         {
             label: 'Blog',
-            url: '#'
+            url: '/blog'
         }
     ];
 
@@ -25,9 +26,11 @@ const Header = () => {
 
     return (
         <div className="flex items-center container mx-auto max-w-screen-xl">
-            <div className="logo frow-0 flex">
-                <Image width="170" src='/logo.svg' alt="Logo"/>
-            </div>
+            <Link href="/">
+                <div className="logo frow-0 flex">
+                    <Image width="170" src='/logo.svg' alt="Logo"/>
+                </div>
+            </Link>
             <nav className="grow flex justify-end">
                 <Menubar className="border-0 pr-0" model={items} end={end} />
             </nav>

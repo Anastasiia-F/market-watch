b1288ae72eb78e9a40aa0197af04be9d93d7044c
Anastasiia Fesenko <afesenk@gmail.com>
2024-10-28 19:49:47 +0100

    change theme mdc-dark-indigo -> bootstrap4-dark-blue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.scss";
-import "primereact/resources/themes/mdc-dark-indigo/theme.css";
+import "primereact/resources/themes/bootstrap4-dark-blue/theme.css";
 import 'primeicons/primeicons.css';
 import Header from '@/app/components/header';
 
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
         <html lang="en">
-          <body>
+          <body className="px-7">
               <header className="">
                   <Header />
               </header>
